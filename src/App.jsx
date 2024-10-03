8e023d64f0ec3d05ae6ef75715af98e96ce49c29
Aurélien St <aurelien.strimelle@bstorm.be>
2024-10-03 09:47:00 +0200

    Initial commit 🤺

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import icon from '/icon.png';
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <img src={icon} alt='Weather logo app' />
+        <span>Exo 07 - App Météo</span>
+      </header>
+      <main>
+        TODO : Create the app :o
+      </main>
+    </>
+  )
+}
+
+export default App
